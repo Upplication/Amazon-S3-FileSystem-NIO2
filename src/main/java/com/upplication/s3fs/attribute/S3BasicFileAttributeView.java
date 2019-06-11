8354bea169a4022e99f7f234c5bf1d69c8488302
labkey-jeckels <jeckels@labkey.com>
2019-06-10 21:20:21 -0700

    Ticket 37594: Error uploading files > 5GB to a S3 enabled folder

--- src/main/java/com/upplication/s3fs/attribute/S3BasicFileAttributeView.java
+++ src/main/java/com/upplication/s3fs/attribute/S3BasicFileAttributeView.java
@@ -1,14 +1,15 @@
 package com.upplication.s3fs.attribute;
 
 import com.amazonaws.services.s3.AmazonS3;
-import com.amazonaws.services.s3.model.CopyObjectRequest;
-import com.amazonaws.services.s3.model.ObjectMetadata;
+import com.amazonaws.services.s3.model.*;
 import com.upplication.s3fs.S3Path;
 
 import java.io.IOException;
 import java.nio.file.attribute.BasicFileAttributeView;
 import java.nio.file.attribute.BasicFileAttributes;
 import java.nio.file.attribute.FileTime;
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class S3BasicFileAttributeView implements BasicFileAttributeView {
@@ -57,11 +58,62 @@ public class S3BasicFileAttributeView implements BasicFileAttributeView {
         // S3 doesn't let you modify attributes of existing objects. However, you can set them as part of a copy
         // operation, and the copy's source and target locations can be identical. This means we don't have to download
         // and reupload the object, which is nice.
-        CopyObjectRequest request = new CopyObjectRequest(bucketName, fileKey, bucketName, fileKey)
-                .withSourceBucketName(bucketName)
-                .withSourceKey(fileKey)
-                .withNewObjectMetadata(metadataCopy);
 
-        client.copyObject(request);
+        long objectSize = metadataCopy.getContentLength();
+        // S3 doesn't support single-copy requests when the object is >5GB, so use a multi-part approach when needed
+        // Taken from https://docs.aws.amazon.com/AmazonS3/latest/dev/CopyingObjctsUsingLLJavaMPUapi.html
+        if (objectSize > 5_000_000_000L) {
+
+            InitiateMultipartUploadRequest initRequest = new InitiateMultipartUploadRequest(bucketName, fileKey).withObjectMetadata(metadataCopy);
+            InitiateMultipartUploadResult initResult = client.initiateMultipartUpload(initRequest);
+
+            long partSize = 5 * 1024 * 1024;
+            long bytePosition = 0;
+            int partNum = 1;
+            List<CopyPartResult> copyResponses = new ArrayList<>();
+            while (bytePosition < objectSize) {
+                // The last part might be smaller than partSize, so check to make sure
+                // that lastByte isn't beyond the end of the object.
+                long lastByte = Math.min(bytePosition + partSize - 1, objectSize - 1);
+
+                // Copy this part.
+                CopyPartRequest copyRequest = new CopyPartRequest()
+                        .withSourceBucketName(bucketName)
+                        .withSourceKey(fileKey)
+                        .withDestinationBucketName(bucketName)
+                        .withDestinationKey(fileKey)
+                        .withUploadId(initResult.getUploadId())
+                        .withFirstByte(bytePosition)
+                        .withLastByte(lastByte)
+                        .withPartNumber(partNum++);
+                copyResponses.add(client.copyPart(copyRequest));
+                bytePosition += partSize;
+            }
+
+            // Complete the upload request to concatenate all uploaded parts and make the copied object available.
+            CompleteMultipartUploadRequest completeRequest = new CompleteMultipartUploadRequest(
+                    bucketName,
+                    fileKey,
+                    initResult.getUploadId(),
+                    getETags(copyResponses));
+            client.completeMultipartUpload(completeRequest);
+        }
+        else {
+            CopyObjectRequest request = new CopyObjectRequest(bucketName, fileKey, bucketName, fileKey)
+                    .withSourceBucketName(bucketName)
+                    .withSourceKey(fileKey)
+                    .withNewObjectMetadata(metadataCopy);
+
+            client.copyObject(request);
+        }
+    }
+
+    // This is a helper function to construct a list of ETags.
+    private List<PartETag> getETags(List<CopyPartResult> responses) {
+        List<PartETag> etags = new ArrayList<>();
+        for (CopyPartResult response : responses) {
+            etags.add(new PartETag(response.getPartNumber(), response.getETag()));
+        }
+        return etags;
     }
 }
